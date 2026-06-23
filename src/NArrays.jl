12ea9eb05946892114723d4c35d491f7e40aeeb5
Gortaf <nicolas.jacquin@gmail.com>
2026-06-23 14:53:42 -0400

    Initial Commit

---
+++ src/NArrays.jl
@@ -0,0 +1,9 @@
+module NArrays
+
+include("DeltaArray.jl")
+include("PackedArray.jl")
+
+export DeltaArray, encode!, searchfirst
+export PackedArray, bitsizeof, prepack, get_val, new_word!, permdedup,update_word_bitmap!, word_bitmap_slice, word_last_set
+
+end # module NArrays
