3daccece3d4de5eba4630e6496303e5919e22467
Gortaf <nicolas.jacquin@gmail.com>
2026-06-23 15:14:52 -0400

    Minor changes to exports

--- src/NArrays.jl
+++ src/NArrays.jl
@@ -6,5 +6,7 @@ include("DeltaArray.jl")
 include("PackedArray.jl")
 
 export DeltaArray, encode!, searchfirst
-export PackedArray, bitsizeof, prepack, get_val, new_word!, permdedup,update_word_bitmap!, word_bitmap_slice, word_last_set
+export PackedArray, bitsizeof, prepack, get_val, new_word!, permdedup, update_word_bitmap!, word_bitmap_slice, word_last_set
+export psortperm, psort!
+
 end # module NArrays
